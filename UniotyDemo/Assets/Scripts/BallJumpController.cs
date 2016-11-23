a4891a3fe99c701e54dfc404679bea8934dd7aff
libertylocked <unlock.liberty@gmail.com>
2016-11-23 00:13:03 -0600

    Save control state for continuous flying motion

--- UniotyDemo/Assets/Scripts/BallJumpController.cs
+++ UniotyDemo/Assets/Scripts/BallJumpController.cs
@@ -5,24 +5,37 @@ public class BallJumpController : MonoBehaviour
 {
     UniotyMaster uniotyMaster;
     Rigidbody rb;
+    bool flyUp = false;
+
+    public byte DeviceID = 0x01;
+    public byte ControlID = 0x01;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
         uniotyMaster = FindObjectOfType<UniotyMaster>();
-        uniotyMaster.GetDeviceControl(0x01, 0x01).InputReceived += OnInputReceived;
+        uniotyMaster.GetDeviceControl(DeviceID, ControlID).InputReceived += OnInputReceived;
     }
 
     void Update ()
     {
-    
+        if (flyUp)
+        {
+            rb.velocity = (Vector3.up * 5.0f);
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Unsubscribe to input event
+        // This does not remove the control from master's control map
+        uniotyMaster.GetDeviceControl(DeviceID, ControlID).InputReceived -= OnInputReceived;
     }
 
     void OnInputReceived(object sender, InputReceivedEventArgs e)
     {
-        if (e.State == 0x01)
-        {
-            rb.velocity = (Vector3.up * 10.0f);
-        }
+        // Move the ball upwards if the control state is 0x01 (pressed)
+        // This event may not be raised every frame - need to save the state
+        flyUp = e.State == 0x01;
     }
 }
